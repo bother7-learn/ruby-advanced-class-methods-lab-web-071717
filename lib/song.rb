1ec61ea5586c9ad799f5d28ecfef1bc58794a56b
bother7 <github email address>
2017-07-19 22:03:21 -0400

    Done

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,94 @@
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
 
-  def self.all
-    @@all
-  end
+def initialize(name=nil, artist=nil)
+@@all << self
+@name = name
+@artist_name = artist
+end
 
-  def save
-    self.class.all << self
+# def artist_name= (name)
+# @artist_name = name
+# end
+
+def self.all
+  @@all
+end
+
+def save
+  self.class.all << self
+  ##binding.pry
+end
+
+def self.create
+  self.new
+
+
+end
+
+def self.new_by_name(name)
+  self.new(name)
+
+end
+
+def self.create_by_name(name)
+ self.new(name)
+# @@all << self
+end
+
+def self.find_by_name(name)
+  ##binding.pry
+  chosen = @@all.select do |song|
+    song.name == name
+  end[0]
+end
+
+def self.find_or_create_by_name(name)
+  if self.find_by_name(name)
+    self.find_by_name(name)
+  else
+    self.create_by_name(name)
   end
+end
+
+def self.alphabetical
+@@all.sort_by {|song| song.name}
+  # binding.pry
+
+end
+
+
+def self.new_from_filename(file)
+array = file.split(" - ")
+array[1].sub!(".mp3", "")
+# binding.pry
+self.new(array[1], array[0])
+
+# self.artist_name = array[0]
+
+  # binding.pry
+
+
+end
+
+def self.create_from_filename(file)
+  array = file.split(" - ")
+  array[1].sub!(".mp3", "")
+  # binding.pry
+  self.new(array[1], array[0])
+
+  # self.artist_name = array[0]
+
+    # binding.pry
+
+end
+
+def self.destroy_all
+  @@all = []
+end
+
 
 end
